f0f2fdc71f226e7a00589d1e210b7f740b893511
LeoDJ <leandro.spaeth@gmail.com>
2020-06-26 01:53:17 +0200

    add configuration for LED palmtree

--- src/config_sample.h
+++ src/config_sample.h
@@ -1,6 +1,5 @@
 // rename to config.h
 
-#define MY_HOSTNAME     "MSE-LED-Arc"
 #define OTA_PASSWORD    ""
 
 #define WIFI_SSID       ""
--- src/globals.h
+++ src/globals.h
@@ -4,20 +4,34 @@
 
 #define DEBUG Serial
 
-#define LED_PIN     12
-#define NUM_LEDS    150
+// #define DEVICE_ARC
+#define DEVICE_PALMTREE
+
+#ifdef DEVICE_ARC
+    #define LED_PIN     12
+    #define NUM_LEDS    150
+    #define LED_MAPPING {72, 71} // maximum 170 LEDs per ArtNet universe
+    #define INIT_STATE  false
+    #define MY_HOSTNAME "MSE-LED-Arc"
+#endif
+#ifdef DEVICE_PALMTREE
+    #define LED_PIN     12
+    #define NUM_LEDS    50
+    #define LED_MAPPING {NUM_LEDS}
+    #define INIT_STATE true
+    #define MY_HOSTNAME "MSE-LED-Palmtree"
+#endif
+
 #define BRIGHTNESS  255
 #define LED_TYPE    WS2811
 #define COLOR_ORDER BRG
 extern CRGB leds[];
-#define INIT_STATE false
 #define INIT_BRIGHTNESS  255
 #define INIT_COLOR  0xFFFFFF
 #define UPDATES_PER_SECOND 60
 
 #define ANIMATION_USE_FASTLED
 
-#define LED_MAPPING {72, 71} // maximum 170 LEDs per ArtNet universe
 extern uint16_t ledMapping[];
 extern uint8_t ledGroups;
 
@@ -26,6 +40,11 @@ extern uint8_t ledGroups;
 #define MIC_PIN 36 // GPIO36 = VP
 #define BUILTIN_LED 2
 
-#define BROKER_IP "mse-pi.lan"
+#define BROKER_IP "mse-pi.makerspace.experimenta.science"
 
 extern bool otaRunning;
+
+
+#if defined(DEVICE_ARC) && defined(DEVICE_PALMTREE)
+    #error Please only choose one device configuration at a time
+#endif
