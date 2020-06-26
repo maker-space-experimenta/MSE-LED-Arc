84a768606c8f7c4da243a73d4d02f16644fa63b7
LeoDJ <leandro.spaeth@gmail.com>
2020-06-26 02:46:47 +0200

    fix color order

--- src/globals.h
+++ src/globals.h
@@ -13,18 +13,19 @@
     #define LED_MAPPING {72, 71} // maximum 170 LEDs per ArtNet universe
     #define INIT_STATE  false
     #define MY_HOSTNAME "MSE-LED-Arc"
+    #define COLOR_ORDER BRG
 #endif
 #ifdef DEVICE_PALMTREE
     #define LED_PIN     12
-    #define NUM_LEDS    50
+    #define NUM_LEDS    26
     #define LED_MAPPING {NUM_LEDS}
     #define INIT_STATE true
     #define MY_HOSTNAME "MSE-LED-Palmtree"
+    #define COLOR_ORDER RGB
 #endif
 
 #define BRIGHTNESS  255
 #define LED_TYPE    WS2811
-#define COLOR_ORDER BRG
 extern CRGB leds[];
 #define INIT_BRIGHTNESS  255
 #define INIT_COLOR  0xFFFFFF
